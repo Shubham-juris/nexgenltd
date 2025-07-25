b6914f7653f300319a1692dbcf261f378eb30bc9
Vinay730 <vinay.juris@gmail.com>
2025-07-25 14:10:07 +0100

    cHANGES

--- src/Components/groceries/GroceriesHome.jsx
+++ src/Components/groceries/GroceriesHome.jsx
@@ -8,7 +8,7 @@ const GroceriesHome = () => {
         className="relative bg-cover bg-center min-h-screen flex items-center justify-center text-white px-6 py-12"
         style={{ backgroundImage: `url(${bgImage})` }}
       >
-        <div className="absolute inset-0 bg-black opacity-50"></div>
+        <div className="absolute inset-0 bg-black opacity-50 "></div>
       </div>
     </>
   );
